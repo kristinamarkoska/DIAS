38cf7518b11086074ab9da5c4c0a4cce89c29fa2
kristinamarkoska <165588196+kristinamarkoska@users.noreply.github.com>
2024-11-13 22:15:36 +0100

    Delete Домашна 1-Дизајн и архитектура на софтвер/домашна1 directory

--- Домашна 1-Дизајн и архитектура на софтвер/домашна1/src/main/java/com/example/project1/data/pipeline/Filter.java
+++
@@ -1,8 +0,0 @@
-package com.example.project1.data.pipeline;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-public interface Filter<T> {
-    T execute(T input) throws IOException, ParseException;
-}
